8b8d1dedc1eb02594aba28fe0ecb3bdef35c0cf2
Mitch Ryan <mitch.ryan@outlook.co.nz>
2017-04-19 20:05:30 +1200

    Add barebones dev setup with react hot loader

---
+++ bin/webpack.config.js
@@ -0,0 +1,116 @@
+const webpack = require('webpack');
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const config = {
+    entry: {
+        'main.js': [
+            './resources/js/main.js',
+        ],
+    },
+
+    output: { 
+        path: path.resolve(process.cwd(), 'public/dist'),
+        publicPath: 'http://localhost:3000/dist',
+        filename: '[name]',
+    },
+
+    resolve: {
+        modules: [
+            'node_modules',
+            path.join(process.cwd(), 'resources/js'),
+            path.join(process.cwd(), 'resources/scss'),
+        ]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: [
+                    'react-hot-loader/webpack',
+                    {
+                        loader: 'babel-loader',
+                        query: {
+                            presets: ['es2015', 'stage-2', 'react']
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [
+                                    require('autoprefixer')({
+                                        browsers: [
+                                            'last 2 versions',
+                                            'ie >= 10',
+                                        ],
+                                    }),
+                                    require('cssnano')
+                                ];
+                            }
+                        }
+                    },
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+};
+
+if (process.env.HOT) {
+    // Add plugins required for HMR.
+    config.plugins = (config.plugins || []).concat([
+        new webpack.HotModuleReplacementPlugin()
+    ]);
+
+    // Add entrypoints required for HMR.
+    for (entry in config.entry) {
+        config.entry[entry] = [
+            'react-hot-loader/patch',
+            'webpack-hot-middleware/client?noInfo=true&reload=true&path=//localhost:3000/__webpack_hmr',
+        ].concat(config.entry[entry]);
+    }
+
+    // Path needs to be '/' for HMR.
+    config.output.path = '/';
+
+    config.output.filename = '[name]';
+} else {
+    config.plugins = config.plugins.concat([
+        function() {
+            this.plugin("done", function(stats) {
+                require("fs").writeFileSync(
+                    path.join(process.cwd(), "stats.json"),
+                    JSON.stringify(stats.toJson().assetsByChunkName)
+                );
+            });
+        },
+    ]);
+}
+
+if (process.env.DEV) {
+    // Enable sourcemaps for dev.
+    config.devtool = "cheap-eval-source-map";
+}
+
+if (process.env.UGLIFY) {
+    config.plugins = (config.plugins || []).concat([
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production'),
+            },
+        }),
+        new UglifyJSPlugin(),
+    ]);
+}
+
+module.exports = config;
---
+++ resources/js/components/App.js
@@ -0,0 +1,17 @@
+import 'main.scss';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Mitch Ryan</h1>
+                <h1>Mitch Ryan</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
