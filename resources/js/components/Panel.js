a568b5efb69e956c9682fd0b4879c0737ef31f2b
Mitch Ryan <mitch.ryan@outlook.co.nz>
2017-04-24 20:47:33 +1200

    Typo

--- resources/js/components/Panel.js
+++ resources/js/components/Panel.js
@@ -6,7 +6,7 @@ export default function() {
             <div className="panel__inner">
                 <h1>Mitch Ryan</h1>
                 <p>New Zealand based full-stack web developer, specialising in interactive media and particularly interested in projects of social importance.</p>
-                <p>Mitch has experience working with major domestic and international organisations, and his work has been recognised by such reputable institutuions as &ldquo;W3&rdquo;, &ldquo;Awwwards&rdquo;, &ldquo;The FWA&rdquo; and &ldquo;The Webby Awards&rdquo;.</p>
+                <p>Mitch has experience working with major domestic and international organisations, and his work has been recognised by such reputable institutions as &ldquo;W3&rdquo;, &ldquo;Awwwards&rdquo;, &ldquo;The FWA&rdquo; and &ldquo;The Webby Awards&rdquo;.</p>
             </div>
         </div>
     )
