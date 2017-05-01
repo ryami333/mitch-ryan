7a222b683c61a3b9da2e648a16af7e7320ee8aec
Mitch Ryan <mitch.ryan@outlook.co.nz>
2017-05-01 21:24:47 +1200

    Update styling using a new function called 'between'

--- resources/js/components/Panel.js
+++ resources/js/components/Panel.js
@@ -5,8 +5,10 @@ export default function() {
         <div className="panel">
             <div className="panel__inner">
                 <h1>Mitch Ryan</h1>
-                <p>New Zealand based full-stack web developer, specialising in interactive media and particularly interested in projects of social importance.</p>
-                <p>Mitch has experience working with major domestic and international organisations, and his work has been recognised by such reputable institutions as &ldquo;W3&rdquo;, &ldquo;Awwwards&rdquo;, &ldquo;The FWA&rdquo; and &ldquo;The Webby Awards&rdquo;.</p>
+                <div className="panel__copy">
+                    <p>New Zealand based full-stack web developer, specialising in interactive media and particularly interested in projects of social importance.</p>
+                    <p>Mitch has experience working with major domestic and international organisations, and his work has been recognised by such reputable institutions as &ldquo;W3&rdquo;, &ldquo;Awwwards&rdquo;, &ldquo;The FWA&rdquo; and &ldquo;The Webby Awards&rdquo;.</p>
+                </div>
                 <div className="panel__links">
                     <a href="#" className="panel__link panel__link--linkedin" />
                     <a href="#" className="panel__link panel__link--email" />
