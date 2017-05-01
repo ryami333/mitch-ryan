58295a25366d2af28b30c7ef7eedf1d4a32ed8c1
Mitch Ryan <mitch.ryan@outlook.co.nz>
2017-05-01 21:33:26 +1200

    Add real links

--- resources/js/components/Panel.js
+++ resources/js/components/Panel.js
@@ -10,8 +10,8 @@ export default function() {
                     <p>Mitch has experience working with major domestic and international organisations, and his work has been recognised by such reputable institutions as &ldquo;W3&rdquo;, &ldquo;Awwwards&rdquo;, &ldquo;The FWA&rdquo; and &ldquo;The Webby Awards&rdquo;.</p>
                 </div>
                 <div className="panel__links">
-                    <a href="#" className="panel__link panel__link--linkedin" />
-                    <a href="#" className="panel__link panel__link--email" />
+                    <a href="https://www.linkedin.com/in/mitch-ryan-2a0b70127/" className="panel__link panel__link--linkedin" target="_BLANK" />
+                    <a href="mailto:ohai@mitch-ryan.com" className="panel__link panel__link--email" />
                 </div>
             </div>
         </div>
