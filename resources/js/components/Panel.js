f945213b66c56a8f8cee9dfcfe46f9401380a240
Mitch Ryan <mitch.ryan@outlook.co.nz>
2017-05-08 22:40:45 +1200

    Fix characters encoding bad

--- resources/js/components/Panel.js
+++ resources/js/components/Panel.js
@@ -6,8 +6,8 @@ export default function() {
             <div className="panel__inner">
                 <h1>Mitch Ryan</h1>
                 <div className="panel__copy">
-                    <p>New Zealand based full-stack web developer, specialising in interactive media and particularly interested in projects of social importance.</p>
-                    <p>Mitch has experience working with major domestic and international organisations, and his work has been recognised by such reputable institutions as &ldquo;W3&rdquo;, &ldquo;Awwwards&rdquo;, &ldquo;The FWA&rdquo; and &ldquo;The Webby Awards&rdquo;.</p>
+                    <p dangerouslySetInnerHTML={{__html: "New Zealand based full-stack web developer, specialising in interactive media and particularly interested in projects of social importance."}} />
+                    <p dangerouslySetInnerHTML={{__html: "Mitch has experience working with major domestic and international organisations, and his work has been recognised by such reputable institutions as “W3”, “Awwwards”, “The FWA” and “The Webby Awards”"}} />
                 </div>
                 <div className="panel__links">
                     <a href="https://www.linkedin.com/in/mitch-ryan-2a0b70127/" className="panel__link panel__link--linkedin" target="_BLANK" />
