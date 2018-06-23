50972a6cd1ce7cce1d025b4b4b1755db07a4c94d
Mitch Ryan <mitch.ryan@outlook.co.nz>
2018-06-23 22:08:16 +1200

    Refactor Triangle to have Flow types and memory optimizations

--- src/js/components/Triangle.js
+++ src/js/components/Triangle.js
@@ -12,65 +12,51 @@ class Triangle extends Component<TriangleProps> {
     componentDidMount() {
         window.addEventListener('mousemove', this.handleMove);
 
-        if (this.ref) {
-            this.ref.width = this.props.innerWidth;
-            this.ref.height = this.props.innerHeight;
-        }
-
-        fastdom.mutate(() => this.mutate());
+        fastdom.mutate(this.mutate);
     }
 
     componentWillUnmount() {
         window.removeEventListener('mousemove', this.handleMove);
     }
 
-    raf: ?Function;
+    componentDidUpdate() {
+        fastdom.mutate(this.mutate);
+    }
+
     ref: ?HTMLCanvasElement;
     context: ?CanvasRenderingContext2D;
     position: number = 0.5;
 
-    measure(e) {
+    measure = (e: SyntheticMouseEvent<*>) => {
         const rx = 1 - e.clientX / this.props.innerWidth;
         const ry = e.clientY / this.props.innerHeight;
         this.position = rx + ry - 0.5;
-    }
+    };
 
-    mutate() {
+    mutate = () => {
         if (this.context) {
-            const { context } = this;
+            const { context, position } = this;
+            const { innerHeight, innerWidth } = this.props;
 
             // Draw BG
             context.fillStyle = '#DDD';
-            context.fillRect(
-                0,
-                0,
-                this.props.innerWidth,
-                this.props.innerHeight,
-            );
+            context.fillRect(0, 0, innerWidth, innerHeight);
+
             // Draw Triangle
             context.beginPath();
             context.fillStyle = '#333';
-            context.moveTo(0, (this.position - 0.5) * this.props.innerHeight);
-            context.lineTo(
-                this.props.innerWidth,
-                this.props.innerHeight * (this.position + 0.5),
-            );
-            context.lineTo(this.props.innerWidth, 0);
+            context.moveTo(0, (position - 0.5) * innerHeight);
+            context.lineTo(innerWidth, innerHeight * (position + 0.5));
+            context.lineTo(innerWidth, 0);
             context.lineTo(0, 0);
-            context.lineTo(0, (this.position - 0.5) * this.props.innerHeight);
+            context.lineTo(0, (position - 0.5) * innerHeight);
             context.fill();
         }
-    }
+    };
 
-    handleMove = e => {
-        this.raf =
-            this.raf ||
-            window.requestAnimationFrame(() => {
-                fastdom.measure(() => this.measure(e));
-                fastdom.mutate(() => this.mutate());
-
-                this.raf = null;
-            });
+    handleMove = (e: SyntheticMouseEvent<*>) => {
+        fastdom.measure(() => this.measure(e));
+        fastdom.mutate(() => this.mutate());
     };
 
     render() {
