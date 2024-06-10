84f23ab83712bc66a41f2fe27fd0302e52271986
Mitch Ryan <ryami333@users.noreply.github.com>
2024-06-10 12:36:14 +0200

    Migrate Panel component to CSS modules

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,76 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
+import classNames from 'classnames/bind';
+import styles from './Panel.module.css';
 
-const Container = styled.div`
-    position: relative;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-height: 100vh;
-    color: white;
-    font-size: 22px;
-    mix-blend-mode: exclusion;
-
-    @media (max-width: 420px) {
-        font-size: 16px;
-    }
-`;
-
-const Heading = styled.h1`
-    font-weight: 900;
-    font-size: 2.66em;
-    color: white;
-`;
-
-const CopyWrapper = styled.div`
-    margin-top: var(--spacing3);
-    display: grid;
-    grid-template-columns: 100%;
-    grid-row-gap: var(--spacing2);
-
-    p {
-        font-size: 1em;
-        line-height: 1.5;
-    }
-
-    a {
-        font-size: 1em;
-        line-height: 1.5;
-        color: inherit;
-        text-decoration: underline;
-    }
-`;
-
-const Inner = styled.div`
-    width: 100%;
-    padding: 0 var(--spacing2);
-    max-width: 890px;
-
-    @media (max-width: 420px) {
-        max-width: 334px;
-    }
-`;
-
-const LinksWrapper = styled.div`
-    margin-top: var(--spacing3);
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    mix-blend-mode: exclusion;
-`;
+const cx = classNames.bind(styles);
 
 function Panel(): React.ReactElement {
     return (
-        <Container>
-            <Inner>
-                <Heading>Mitch Ryan.</Heading>
-                <CopyWrapper>
+        <div className={cx('container')}>
+            <div className={cx('inner')}>
+                <h1 className={cx('heading')}>Mitch Ryan.</h1>
+                <div className={cx('copy-wrapper')}>
                     <p>
-                        New Zealand based full-stack web developer, specialising
-                        in interactive media and particularly interested in
-                        projects of social importance.
+                        Berlin based full-stack web developer from New Zealand,
+                        specialising in interactive media and particularly
+                        interested in projects of social importance.
                     </p>
                     <p>
                         Mitch has experience working with major domestic and
@@ -89,9 +32,9 @@ function Panel(): React.ReactElement {
                     <div>
                         <a href="mailto:ohai@mitch-ryan.com">Email.</a>
                     </div>
-                </CopyWrapper>
-            </Inner>
-        </Container>
+                </div>
+            </div>
+        </div>
     );
 }
 
